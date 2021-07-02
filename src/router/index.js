58898b0346b89df746281c9657cb937315cce4f3
Marco Lin <pohanlin0605@gmail.com>
2021-07-02 10:51:04 +0800

    add authorizationIsAdmin to verify current user's authority

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,15 @@ import store from '../store'
 
 Vue.use(VueRouter)
 
+const authorizationIsAdmin = (to, from, next) => {
+  const currentUser = store.state.currentUser
+  if (currentUser && !currentUser.isAdmin) {
+    next('/not-found')
+    return
+  }
+  next()
+}
+
 const routes = [
   {
     path: '/',
@@ -71,32 +80,38 @@ const routes = [
   {
     path: '/admin/restaurants',
     name: 'admin-restaurants',
-    component: () => import('../views/AdminRestaurants.vue')
+    component: () => import('../views/AdminRestaurants.vue'),
+    beforeEnter: authorizationIsAdmin
   },
   {
     path: '/admin/restaurants/new',
     name: 'admin-restaurant-new',
-    component: () => import('./../views/AdminRestaurantNew.vue')
-  },
-  {
-    path: '/admin/restaurants/:id/edit',
-    name: 'admin-restaurant-edit',
-    component: () => import('./../views/AdminRestaurantEdit.vue')
+    component: () => import('./../views/AdminRestaurantNew.vue'),
+    beforeEnter: authorizationIsAdmin
   },
   {
     path: '/admin/restaurants/:id',
     name: 'admin-restaurant',
-    component: () => import('../views/AdminRestaurant.vue')
+    component: () => import('../views/AdminRestaurant.vue'),
+    beforeEnter: authorizationIsAdmin
+  },
+  {
+    path: '/admin/restaurants/:id/edit',
+    name: 'admin-restaurant-edit',
+    component: () => import('./../views/AdminRestaurantEdit.vue'),
+    beforeEnter: authorizationIsAdmin
   },
   {
     path: '/admin/categories',
     name: 'admin-categories',
-    component: () => import('../views/AdminCategories.vue')
+    component: () => import('../views/AdminCategories.vue'),
+    beforeEnter: authorizationIsAdmin
   },
   {
     path: '/admin/users',
     name: 'admin-users',
-    component: () => import('../views/AdminUsers.vue')
+    component: () => import('../views/AdminUsers.vue'),
+    beforeEnter: authorizationIsAdmin
   },
   {
     path: '*',
